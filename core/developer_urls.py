d0236cf61f850e4ecada6412aee3b5416a771eb2
Robert Kessler <robert.kessler@klarna.com>
2016-10-08 10:40:33 +0200

    Added sample for api

--- core/developer_urls.py
+++ core/developer_urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = [
      url(r'info/$',  core.developer_views.info, name='developer_info'),
+     url(r'v1/username/$',  core.developer_views.protected_api, name='developer_api_test'),
 ]
--- core/developer_views.py
+++ core/developer_views.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render_to_response, render
 
 # Create your views here.
 from django.template import RequestContext
+from django.views.decorators.http import require_http_methods
+from oidc_provider.lib.utils.oauth2 import protected_resource_view
+
 
 @login_required
 def info(request):
     rc = RequestContext(request)
     return render_to_response('developer/info.html', rc)
+
+
+@require_http_methods(['GET'])
+@protected_resource_view(['location'])
+def protected_api(request, *args, **kwargs):
+
+    dic = {
+        'protected': 'information',
+        'username': request.user
+    }
+
+    return JsonResponse(dic, status=200)
