f2e03c32e96cf60c0bed3574cf63fb1d7205e5fc
Robert Kessler <robert.kessler@klarna.com>
2016-10-29 10:50:01 +0200

    #18 Added last online to profiles and user overview

--- profile/models.py
+++ profile/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -17,3 +18,18 @@ class UserProfile(models.Model):
     invited_by = models.ForeignKey(User, related_name='invited_by', null=True)
     about_me_text = models.TextField(max_length=3000, default='')
     avatar_link = models.URLField(blank=True)
+
+    @property
+    def last_online_class(self):
+        if not self.user.last_login:
+            return 'red'
+
+        tdm14 = datetime.datetime.today() - datetime.timedelta(days=14)
+        tdm3 = datetime.datetime.today() - datetime.timedelta(days=3)
+
+        if self.user.last_login.date() < tdm14.date():
+            return 'red'
+        elif tdm14.date() <= self.user.last_login.date() < tdm3.date():
+            return 'yellow'
+        else:
+            return 'green'
