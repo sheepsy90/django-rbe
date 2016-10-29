2b6b3d02478f2195ccadbaed96b8d06dada90c8c
Robert Kessler <robert.kessler@klarna.com>
2016-10-29 15:09:19 +0200

    Added languages fill reminder to reminder sendout

--- profile/management/commands/profile_fill_reminder_sendouts.py
+++ profile/management/commands/profile_fill_reminder_sendouts.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 from library.log import rbe_logger
 from library.mail.ProfileCompletionEmail import ProfileCompletionEmail
 from location.models import Location
-from profile.models import UserProfile
+from profile.models import UserProfile, LanguageSpoken
 
 
 class Command(BaseCommand):
@@ -19,15 +19,16 @@ class Command(BaseCommand):
             return False
 
     def handle(self, *args, **options):
-        all_users = User.objects.all()
+        all_users = User.objects.filter(username='sheepy')
 
         for user in all_users:
             location_missing = not self.has_location(user)
             about_missing = not self.has_about(user)
+            languages_missing = not self.has_languages(user)
 
-            if location_missing or about_missing:
+            if location_missing or about_missing or languages_missing:
                 gec = ProfileCompletionEmail()
-                gec.send(username=user.username, recipient_list=[user.email], location_missing=location_missing, about_missing=about_missing)
+                gec.send(username=user.username, recipient_list=[user.email], location_missing=location_missing, about_missing=about_missing, languages_missing=languages_missing)
                 rbe_logger.info("Sending email to {} for profile fill reminder on Loc:{} / About:{}".format(user.email, location_missing, about_missing))
             else:
                 rbe_logger.info("Skipping email {} - already filled profile.".format(user.email))
@@ -35,3 +36,6 @@ class Command(BaseCommand):
     def has_about(self, user):
         up = UserProfile.objects.get(user=user)
         return len(up.about_me_text) > 50
+
+    def has_languages(self, user):
+        return LanguageSpoken.objects.filter(user=user).count() > 0
