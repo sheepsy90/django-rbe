03cfefb1d4bcecec5c1e552b210574bdb00aa264
Robert Kessler <robert.kessler@klarna.com>
2016-11-12 13:32:22 +0100

    Added Label for Travis

--- core/tests/test_toogles_framework.py
+++ core/tests/test_toogles_framework.py
@@ -66,3 +66,5 @@ class TestTogglesFramework(TestCase):
         self.assertTrue(response.context['user'].is_authenticated())
         self.assertDictEqual(response.context['toggles'], {u'sample_toggle_one': True})
 
+    def test_testing_building_works(self):
+        self.assertTrue(False)
