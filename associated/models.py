3933850c157c7836dea898c21cb3e820b163f0f9
Robert Kessler <robert.kessler@klarna.com>
2016-11-24 22:17:28 +0100

    Added __str__ method for admin page

--- associated/models.py
+++ associated/models.py
@@ -7,3 +7,6 @@ class AssociatedService(models.Model):
     enabled = models.BooleanField(default=False, help_text="If the client is enabled or not.")
     description = models.TextField(default='', help_text="A description of what the client does.")
     logo_url = models.URLField(blank=True, help_text="A URL to the logo of the associated page.")
+
+    def __str__(self):
+        return "{} // {} // {}".format(self.client.name, self.client.website_url, self.enabled)
